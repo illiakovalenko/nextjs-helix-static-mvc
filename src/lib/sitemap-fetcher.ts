11bd68d4dd0b7b49b361cbe7a4ee04313990def0
illiakovalenko <zlatoborodyi@gmail.com>
2021-07-01 12:42:37 +0300

    endpoint

--- src/lib/sitemap-fetcher.ts
+++ src/lib/sitemap-fetcher.ts
@@ -14,6 +14,7 @@ export class SitecoreSitemapFetcher {
   private _disconnectedSitemapService: DisconnectedSitemapService;
 
   constructor() {
+    console.log('!!!!!!!!!!!!!!!!!!!!!!!', config.graphQLEndpoint);
     this._graphqlSitemapService = new GraphQLSitemapService({
       endpoint: config.graphQLEndpoint,
       apiKey: config.sitecoreApiKey,
