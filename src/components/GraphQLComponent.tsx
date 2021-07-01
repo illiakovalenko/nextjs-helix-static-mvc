f76d28781979925c4122a68404fc778ee5cd5b78
illiakovalenko <zlatoborodyi@gmail.com>
2021-07-01 14:57:55 +0300

    push changes

--- src/components/GraphQLComponent.tsx
+++ src/components/GraphQLComponent.tsx
@@ -1,26 +1,30 @@
 import { GraphQLRequestClient } from '@sitecore-jss/sitecore-jss-nextjs';
 import React, { useState } from 'react';
-import { GraphQlComponentQueryDocument } from './GraphQLComponent.graphql';
+import { GraphQlQueryDocument } from './GraphQLComponent.graphql';
 import config from 'temp/config';
 import { StyleguideComponentProps } from 'lib/component-props';
 
 const GraphQLConnectedDemo = (props: StyleguideComponentProps): JSX.Element => {
-  const [state, setState] = useState();
+  const [state, setState] = useState({});
 
   const graphQLClient = new GraphQLRequestClient(config.graphQLEndpoint, {
     apiKey: config.sitecoreApiKey,
   });
 
   graphQLClient
-    .request(GraphQlComponentQueryDocument, {
+    .request(GraphQlQueryDocument, {
       path: props.rendering.dataSource,
     })
-    .then((data) => setState(data as any));
+    .then((data) => {
+      console.log('DATA:', data);
+      setState(data as any);
+      return data;
+    });
 
   return (
     <div>
       <h3>GraphQLComponent</h3>
-      {state ? <div>{JSON.stringify(state)}</div> : <p>.........</p>}
+      <div>{JSON.stringify(state)}</div>
     </div>
   );
 };
